6ac5ca0e64705fe166a2e427e0cffb0031827d47
Aiden Jastrzembski <aidenjastrzembski@gmail.com>
2025-03-31 01:32:12 -0400

    gf

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -18,13 +18,7 @@ export const products = [
     slug: "nucleus",
     content: (
       <div>
-        <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
-        </p>
+        <p>Nucleus is a ...</p>
         <p>
           Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
           Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
