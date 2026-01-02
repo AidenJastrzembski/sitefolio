405928aa8e17b901cbfa3645d8072a8b25f6fa6e
aidenjastrzembski <aidenjastrzembski@gmail.com>
2026-01-02 00:34:07 -0500

    restart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
-<header>
-	<h1>Aiden Jastrzembski</h1>
-</header>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
