74a58bf80f0d2ad60cec135844a4f8f5d137e9b9
AidenJastrzembski <aidenjastrzembski@gmail.com>
2026-03-30 22:56:14 -0400

    start writing out the site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Aiden Jastrzembski';
+export const SITE_DESCRIPTION = 'A place where I can rant about tech';
---
+++ src/content/blog/how-an-art-aesthetic-gives-me-hope.md
@@ -0,0 +1,7 @@
+---
+title: 'How an Art Aesthetic Gives Me Hope'
+description: 'What Solarpunk can teach us'
+pubDate: 'March 30 2026'
+heroImage: '../../assets/blog-placeholder-3.jpg'
+---
+
